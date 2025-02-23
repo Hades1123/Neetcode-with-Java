1553454a59f331577d3740517ac99e2db6e0bae9
Hades1123 <dp1.1a7.si@gmail.com>
2025-02-23 09:31:17 +0700

    2nd commit

--- src/LinkedList/reverseList.java
+++ src/LinkedList/reverseList.java
@@ -6,12 +6,13 @@ class ListNode {
       ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
-
-public class reverseList {
 }
 
-class Solution {
+public class reverseList {
+
+}
+
+class Solution1 {
     public ListNode reverseList(ListNode head) {
         ListNode current = head;
         ListNode prev = null;
