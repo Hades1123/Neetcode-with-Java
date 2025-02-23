25a9b9526299663baee301c822f18ca746282665
Hades1123 <dp1.1a7.si@gmail.com>
2025-02-23 09:06:21 +0700

    first commit

---
+++ src/LinkedList/reverseList.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+public class reverseList {
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        while(current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+}
+
