4d1290cd81ccb08a025bdb79f65a9dc2e7ec62b9
Hades1123 <dp1.1a7.si@gmail.com>
2025-02-23 09:51:21 +0700

    has cycle

---
+++ src/LinkedList/hasCycle.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+public class hasCycle {
+}
+
+class Solution3 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
